0eb8bee2860cb2d010d7066f3475cfb946b8fa20
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-13 20:19:43 +0100

    Imported Redux packages to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import './App.css';
 
 class App extends React.Component {
