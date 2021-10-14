ede4e7e7d70b80ff900f750fae6b1bd08eb1598a
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-14 14:54:07 +0100

    Remove imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
 import './App.css';
 
 class App extends React.Component {
