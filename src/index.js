6a4f1076b9007448cbc4c26c62e32c0ed3fd1c13
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-14 12:06:11 +0100

    Add store and devtools to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
