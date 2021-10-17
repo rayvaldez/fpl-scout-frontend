f35f560a37b9f24b327b224284001b348209eaec
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-17 11:25:06 +0100

    Make successful call to API using componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('https://localhost:3001/api/v1/players/')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
