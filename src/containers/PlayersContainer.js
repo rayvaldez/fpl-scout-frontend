05b9c936fc3bf2c484a552b839a619d82fcbdd3a
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-21 12:44:04 +0100

    Create a class component for Players

---
+++ src/containers/PlayersContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PlayersContainer extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        PlayersContainer
+      </div>
+    )
+  }
+}
+
+export default PlayersContainer
