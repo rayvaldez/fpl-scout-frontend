cad4376be18f22d833e5a2c8a3acdb5e7132c126
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-22 16:55:43 +0100

    Add Player/Comment containers to App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PlayersContainer from './containers/PlayersContainer';
+import CommentsContainer from './containers/CommentsContainer';
 import './App.css';
 
 class App extends React.Component {
-
-  componentDidMount() {
-    fetch('http://localhost:3001/api/v1/players/')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
 
   render() {
     return (
       <div className="App">
         <h1>Fantasy Premier League Scout</h1>
+        <PlayersContainer/>
+        <CommentsContainer/>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
