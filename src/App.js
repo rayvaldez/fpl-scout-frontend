57f1cd2b433bbebf0a12d24fe9e7b70f278e394a
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-29 22:29:40 +0100

    Add comment action to mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {fetchPlayers} from './actions/fetchPlayers';
-import PlayersContainer from './containers/PlayersContainer';
+import { fetchPlayers } from './actions/fetchPlayers';
+import { fetchComments } from './actions/fetchComments';
+import Players from './components/Players';
+import Comments from './components/Comments';
 import CommentsContainer from './containers/CommentsContainer';
 import './App.css';
 
 class App extends React.Component {
 
   componentDidMount() {
-    this.props.fetchPlayers()
+    this.props.fetchPlayers();
+    this.props.fetchComments();
   }
 
   render() {
     return (
       <div className="App">
         <h1>Fantasy Premier League Scout</h1>
-        <PlayersContainer/>
+        {/*<Players players={this.props.players}/>*/}
         <CommentsContainer/>
       </div>
     );
@@ -24,8 +27,9 @@ class App extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    players: state.players
+    players: state.players,
+    comments: state.comments
   }
 }
 
-export default connect(mapStateToProps, {fetchPlayers})(App);
+export default connect(mapStateToProps, {fetchPlayers, fetchComments})(App);
