9b0e25476f4f49a3a54ce90664a86ae02f204d99
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-31 21:57:06 +0000

    Move Comment Components from App.js to commentsContainer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { fetchPlayers } from './actions/fetchPlayers';
-import { fetchComments } from './actions/fetchComments';
-import Players from './components/Players';
-import Comments from './components/Comments';
 import PlayersContainer from './containers/PlayersContainer';
 import CommentsContainer from './containers/CommentsContainer';
 import './App.css';
 
 class App extends React.Component {
-
-  componentDidMount() {
-    this.props.fetchComments();
-  }
 
   render() {
     return (
@@ -26,10 +17,4 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    comments: state.comments
-  }
-}
-
-export default connect(mapStateToProps, { fetchComments })(App);
+export default App;
--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Comments from '../components/Comments';
 import CommentInput from '../components/CommentInput';
-
+import { fetchComments } from '../actions/fetchComments';
 
 class CommentsContainer extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchComments();
+  }
 
   render() {
     return (
       <div>
+        <h5>CommentContainer</h5>
         <Comments/>
         <CommentInput/>
       </div>
-    )
+    );
+  };
+}
+
+
+const mapStateToProps = state => {
+  return {
+    comments: state.comments
   }
 }
 
-export default CommentsContainer
+export default connect(mapStateToProps, { fetchComments })(CommentsContainer);
