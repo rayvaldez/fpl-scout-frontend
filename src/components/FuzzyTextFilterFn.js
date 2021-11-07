acb254ce8ccdae06ab699b667383401b9ffae0b7
rayvaldez <ray_valdez@hotmail.co.uk>
2021-11-07 17:11:05 +0000

    Create fuzzy text filter component

---
+++ src/components/FuzzyTextFilterFn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { matchSorter } from 'match-sorter';
+
+const fuzzyTextFilterFn = (rows, id, filterValue) => {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+fuzzyTextFilterFn.autoRemove = val => !val
+
+export default fuzzyTextFilterFn;
