831e5eb0740f9dad6bbb2e2d29f6833c40993fb2
rayvaldez <ray_valdez@hotmail.co.uk>
2021-11-07 18:35:38 +0000

    Create default filter component to apply to each header

---
+++ src/components/DefaultColumnFilter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const DefaultColumnFilter = ({
+  column: { filterValue, preFilteredRows, setFilter },
+}) => {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
+
+export default DefaultColumnFilter;
