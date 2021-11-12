324eff6c32fb82f9faa9a67d789c4af9681ded2d
rayvaldez <ray_valdez@hotmail.co.uk>
2021-11-12 22:17:26 +0000

    Create add report case to commentReducer

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -3,6 +3,8 @@ export default function commentReducer(state = {comments: []}, action ) {
   switch (action.type) {
     case 'FETCH_COMMENTS':
       return {comments: action.payload}
+    case 'ADD_REPORT':
+      return {...state, comments: [...state.comments, action.payload]}
     default:
       return state
   }
