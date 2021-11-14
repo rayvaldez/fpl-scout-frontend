3c7b6c889c2dabdf91ffa96eb650d9e9fd26a678
rayvaldez <ray_valdez@hotmail.co.uk>
2021-11-14 13:25:48 +0000

    Move Comment container to Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Team from './Team';
-
+import CommentsContainer from '../containers/CommentsContainer'
 
 const Player = (props) => {
 
@@ -13,8 +13,19 @@ const Player = (props) => {
           <img src={`http://resources.premierleague.com/premierleague/photos/players/110x140/p${player.photo.substring(0, player.photo.length-4)}.png`} alt={`${player.web_name}`}/>
           <h4>{player.first_name} {player.second_name}</h4>
           <Team team={player.team}/>
+          <li>Cost - {player.now_cost}</li>
+          <li>Points per game - {player.points_per_game}</li>
+          <li>Total Points - {player.total_points}</li>
+          <li>Goals - {player.goals_scored}</li>
+          <li>Assists - {player.assists}</li>
+          <li>Clean Sheets - {player.clean_sheets}</li>
+          <li>Bonus Points - {player.bonus}</li>
         </div>
       : <div>Loading...</div>}
+
+      <br/>
+
+      <CommentsContainer /><br/>
     </div>
   )
 
